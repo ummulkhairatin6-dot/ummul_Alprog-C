16e7de8be4bda937d4fc6c51d745a7cea1e626ca
Ummul_Alprog-C <ummulkhairatin6@gmail.com>
2025-11-13 14:56:06 +0800

    Add files via upload

---
+++ meet3 (1).py
@@ -0,0 +1,17 @@
+# tipe data = tipe dari sebuah nilai
+
+x = 10 # tipe data integer
+print("nilai dari x=",x)
+print("tipe data dari x=",type(x))
+
+y = 3.5 # tipe data float
+print("nilai dari y=",y)
+print("tipe data dari y=",type(x))
+
+nama ="ratin" # tipe data string
+print("nilai dari nama=",nama)
+print("tipe data dari nama=",type(nama ))
+
+kebenaran =True # tipe data bolean
+print("nilai dari kebenaran=",kebenaran)
+print("tipe data dari kebenaran=",type(kebenaran))
