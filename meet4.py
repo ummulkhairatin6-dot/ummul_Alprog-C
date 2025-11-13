1507e203cf0c4e4a0c67dc67dfd0370e5797ba53
Ummul_Alprog-C <ummulkhairatin6@gmail.com>
2025-11-13 14:56:52 +0800

    Add files via upload

---
+++ meet4.py
@@ -0,0 +1,20 @@
+# mengambil input dari user
+
+nama = input("masukkan nama anda:")
+print ("selamat datang",nama)
+print("type(nama)")
+
+#jika ingin mengambil data integer, maka
+data_int =int(input) ("masukkan data anda:")
+print("nilai anda",data_int)
+print ("type(data_int) ")
+
+#jika ingin mengambil data integer, maka
+data float =float(input("masukkan data anda:")
+print("nilai anda",data_float)
+print(type(data_float))
+
+#jika ingin mengambil data integer, maka
+data_bool = bool(input("masukkan data anda:"))
+print("nilai anda",data_bool)
+print(type(data_bool))
