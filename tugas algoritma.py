1d58feb1b566e835aae2b59abc248b3b5da2694d
Ummul_Alprog-C <ummulkhairatin6@gmail.com>
2025-11-15 19:39:19 +0800

    Update tugas algoritma.py

--- tugas algoritma.py
+++ tugas algoritma.py
@@ -1,11 +1,12 @@
 # tugas : buatlah program dengan output sebagai berikut :
 
-nama = input("masukan nama anda")
+nama = input("ummul khairatin")
 print("nama saya adalah ", nama)
 print(type(nama))
 
-umur = input(" masukan umur anda")
+umur = input(" 18")
 print("umur saya adalah", umur)
 print(type(umur))
+
 
 
