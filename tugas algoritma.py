6dba735663e31d12c3b014af2d967344ed80d193
Ummul_Alprog-C <ummulkhairatin6@gmail.com>
2025-11-15 19:37:39 +0800

    Add files via upload

---
+++ tugas algoritma.py
@@ -0,0 +1,11 @@
+# tugas : buatlah program dengan output sebagai berikut :
+
+nama = input("masukan nama anda")
+print("nama saya adalah ", nama)
+print(type(nama))
+
+umur = input(" masukan umur anda")
+print("umur saya adalah", umur)
+print(type(umur))
+
+
