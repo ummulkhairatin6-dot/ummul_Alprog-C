5f9a209e1dbef7f5a1da0dbc5397ba85c8939306
Ummul_Alprog-C <ummulkhairatin6@gmail.com>
2025-11-27 15:37:06 +0800

    Add files via upload

---
+++ meet5.py
@@ -0,0 +1,51 @@
+#operasi aritmetika
+#x/+-
+#dalam pemrograman lambang x menjadi "*"dan :menjadi "/"dan :""
+
+#penjumlahan lambang (+)
+a=13
+b=9
+nilai=a+b
+print(a,"+",b,"=",nilai)
+
+#pengurangan lambang (-)
+a=24
+b=2
+nilai=a-b
+print(a, "-",b, "=",nilai)
+
+#perkalian lambang (*)
+a=6
+b=4
+nilai=a * b
+print(a, "*",b, "=", nilai)
+
+#pembagian lambang (/)
+a=32
+b=2
+nilai=a/b
+print(a, "/",b,"=",nilai)
+
+#perpangkatan lambang (**) 
+a=3**8
+b=3**6
+nilai=a**b
+print(a, "**",b,"=",nilai)
+
+#modulus lambang (%) (sisa bagai)
+a=30
+b=4
+nilai=a%b
+print(a,"%",b,"=",nilai)
+
+#floor division lambang (//) (pembulatan)
+a=(4)
+b=(2)
+nilai=a//b
+print(a,"//",b"=",nilai)
+
+#operasi prioritas
+#1.()
+#2.perpangkatan akar
+#3.x/
+#4.+-
