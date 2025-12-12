febc70b6a5a0f3930ce946a7f60724a5d661a624
Ummul_Alprog-C <ummulkhairatin6@gmail.com>
2025-12-12 19:59:44 +0800

    Add files via upload

---
+++ meet7.py
@@ -0,0 +1,58 @@
+# komparasi = true / false
+# > < >= <= != == is is-not
+
+# komparasi dari (>) = lebih besar
+print("========= lebih dari (>)")
+x = 25
+y = 86
+hasil = x > 8
+print(x, ">", 8, "=", hasil)
+
+# komparasi nilai (<) = lebih kecil
+print("========= kurang dari (<)")
+x = 25
+y = 86
+hasil = x < 8
+print(x, "<", 8, "=", hasil)
+
+# komparasi dari (>=) = lebih besar dari 
+print("========= lebih dari sama dengan (>=)")
+x = 25
+y = 86
+hasil = x >= 8
+print(x, ">=", 8, "=", hasil)
+
+# komparasi dari (<=) = lebih kecil dari 
+print("========= kurang dari sama dengan (<=)")
+x = 25
+y = 86
+hasil = x <= 8
+print(x, "<=", 8, "=", hasil)
+
+# komparasi dari (!=) = tidak sama dgn
+print("========= tidak sama dengan (!=)")
+x = 25
+y = 86
+hasil = x != 8
+print(x, "!=", 8, "=", hasil)
+
+# komparasi dari (==) = perbandingan
+print("========= sama dengan sama dengan (==)")
+x = 25
+y = 86
+hasil = x == y
+print(x, "==", y, "=", hasil)
+
+# komparasi dari (is) = perbandingan
+print("========= perbandingan (is)")
+x = 25
+y = 86
+hasil = x is y
+print(x, "is", y, "=", hasil)
+
+# komparasi dari (is not)
+print("========= tidak sama dengan (is not)")
+x = 25
+y = 86
+hasil = x is not y
+print(x, "is not", y, "=", hasil) 
